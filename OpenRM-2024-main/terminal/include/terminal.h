eceb8eb820c38893ebbe1ca5993fe2df456e3788
pluto <2149972901@qq.com>
2024-09-27 22:13:54 +0800

    Revert "cuda -> opencv"
    This reverts commit 0df2f9376279e3eedb86401992145efc4ea57b79.

--- OpenRM-2024-main/terminal/include/terminal.h
+++
@@ -1,8 +0,0 @@
-#ifndef __OPENRM_TERMINAL_H__
-#define __OPENRM_TERMINAL_H__
-
-#include <openrm.h>
-
-void readFile(const std::string &path, std::vector<double>& inputs);
-
-#endif
--- OpenRM-2024-main/terminal/run.sh
+++
@@ -1,9 +0,0 @@
-# !bin/bash
-
-max_threads=$(cat /proc/cpuinfo | grep "processor" | wc -l)
-cd build
-cmake ..
-make -j "$max_threads"
-make install
-
-
